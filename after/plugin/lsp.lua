06374f95dca4d5468e5c1c940cf678cea197795b
Puneet Sharma <puneetsharma9@hotmail.com>
2025-08-30 18:40:57 +0530

    lsp update

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -12,7 +12,7 @@ lsp.nvim_workspace()
 
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -28,18 +28,18 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
 })
 
 -- Define common LSP on_attach function
 local function lsp_on_attach(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -47,11 +47,11 @@ local function lsp_on_attach(client, bufnr)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-  
+
   -- Ensure capabilities are properly set
   if client.server_capabilities then
     client.server_capabilities.codeActionProvider = true
@@ -81,14 +81,14 @@ nvim_lsp.volar.setup({
   on_attach = function(client, bufnr)
     -- Run the common LSP on_attach first
     lsp_on_attach(client, bufnr)
-    
+
     -- Enable formatting for Volar
     client.server_capabilities.documentFormattingProvider = true
     client.server_capabilities.documentRangeFormattingProvider = true
-    
-    local opts = {buffer = bufnr, remap = false}
+
+    local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end, opts)
-    
+
     -- Auto-format on save for Vue files
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
@@ -170,25 +170,24 @@ nvim_lsp.tsserver.setup {
 --]]
 
 lsp.on_attach(function(client)
-    if nvim_lsp.util.root_pattern("package.json")(vim.fn.getcwd()) then
-        if client.name == "denols" then
-            client.stop()
-            return
-        end
+  if nvim_lsp.util.root_pattern("package.json")(vim.fn.getcwd()) then
+    if client.name == "denols" then
+      client.stop()
+      return
     end
+  end
 end)
 
 vim.g.markdown_fenced_languages = {
-    "ts=typescript"
+  "ts=typescript"
 }
 
 -- Just need to set the directory for denols to startup in
 -- if it detects either files thats what it will do
 lsp.configure('denols', {
-    root_dir = nvim_lsp.util.root_pattern("deno.json", "import_map.json"),
+  root_dir = nvim_lsp.util.root_pattern("deno.json", "import_map.json"),
 })
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
-
