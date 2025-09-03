1de8101bde3ffcd3f5c8d897aeef13e9c7551f96
puneetxp <puneet@dreamcast.co>
2025-09-03 20:49:40 +0530

    Update set with ssh clipboard

--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -33,3 +33,23 @@ vim.opt.colorcolumn = "200"
 -- Timeout for key sequences (for multi-key leader mappings)
 vim.opt.timeoutlen = 1000
 
+-- OSC52 clipboard configuration for SSH sessions and remote work
+-- This enables clipboard functionality when working over SSH
+-- OSC52 allows copying to system clipboard through terminal escape sequences
+if vim.env.SSH_TTY or vim.env.SSH_CLIENT or vim.env.SSH_CONNECTION then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+else
+  -- For local sessions, use system clipboard if available
+  vim.opt.clipboard = "unnamedplus"
+end
+
